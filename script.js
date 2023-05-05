5eed15534e1f6e3ad1cdb02f232867c95851ceee
ᎮᏬᏝᏕᏋᏕ <128443881+pulsedrift@users.noreply.github.com>
2023-05-04 21:47:01 -0400

    Update script.js
    Now it actually works (again I messed it up the first time)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 tanks[0].health=Infinity;
-let TANK_FORWARD_SPEED = parseInt(prompt("Enter tank forward speed: "));
-let TANK_REVERSE_SPEED = parseInt(prompt("Enter tank reverse speed: "));
-let SHOOT_DELAY = 0;
-let BULLET_LIFE = parseInt(prompt("Enter bullet life: "));
-let BULLET_SPEED = parseInt(prompt("Enter bullet speed: "));
+TANK_FORWARD_SPEED = parseInt(prompt("Enter tank forward speed: "));
+TANK_REVERSE_SPEED = parseInt(prompt("Enter tank reverse speed: "));
+SHOOT_DELAY = 0;
+BULLET_LIFE = parseInt(prompt("Enter bullet life: "));
+BULLET_SPEED = parseInt(prompt("Enter bullet speed: "));
